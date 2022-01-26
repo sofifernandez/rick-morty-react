5745e008616cda14ed449ffe206763f16566eb9a
Sofia Fernandez <sofiafernandez03@gmail.com>
2022-01-26 11:26:04 -0300

    font configuration

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+import "./NavBar.scss"
+// import { NavLink } from "react-router-dom";
+// import { CartWidget } from "../CartWidget/CartWidget"
+// import logo from '../../images/logo.svg'
+
+
+export const NavBar = () => {
+    return (
+        <header className="header">
+            PRUEBA LETRA HEADER
+        </header>
+    )
+}
